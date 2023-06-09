bf3d8f8a6d037460c66be9d4f9abe3e5aa9a678f
Vikramjit40 <vikramjit@sookshum-labs.com>
2023-06-09 17:04:52 +0530

    Add

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <></>
+   <div className='container'>
+<div className='row'>
+  <div className='col'></div></div>   
+   </div>
   )
 }
